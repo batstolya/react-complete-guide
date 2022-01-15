bfbd3fd5196691dd20ba4c79fbd7e5060b522b1f
DESKTOP-4SO77QD\Анатолий <batstolya@gmail.com>
2022-01-15 21:51:06 +0200

    to chance expenses files

--- src/componets/ExpenseItem.js
+++ src/componets/ExpenseItem.js
@@ -1,17 +1,15 @@
-import './ExpenseItem.css'
-
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 
+function ExpenseItem(props) {
+ 
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+    <div className="expense-item">
+     <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
