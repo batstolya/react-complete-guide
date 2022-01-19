734256163d3418f6f634b7381c0eb66e1080c7aa
DESKTOP-4SO77QD\Анатолий <batstolya@gmail.com>
2022-01-19 14:13:17 +0200

    The Concept of Composition (children props)

--- src/componets/Expenses.js
+++ src/componets/Expenses.js
@@ -1,29 +1,32 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
 
 function Expenses(props) {
-return (
-<div>
-        <ExpenseItem 
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-        />
-        <ExpenseItem 
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-        />
-        <ExpenseItem 
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-        />
-        <ExpenseItem 
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-        />
-</div>)
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
 }
 
 export default Expenses;
